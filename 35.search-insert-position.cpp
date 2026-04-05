f193486664283ef792076f75b796aefed4f267dc
Sebastian Sandstig <sebastian.sandstig@gmail.com>
2026-04-05 20:35:25 +0200

    35

---
+++ 35.search-insert-position.cpp
@@ -0,0 +1,18 @@
+#include <algorithm>
+#include <vector>
+using std::vector;
+/*
+ * @lc app=leetcode id=35 lang=cpp
+ *
+ * [35] Search Insert Position
+ */
+
+// @lc code=start
+class Solution {
+public:
+    int searchInsert(vector<int>& nums, int target) {
+        return std::distance(nums.begin(), std::lower_bound(nums.begin(), nums.end(), target));
+    }
+};
+// @lc code=end
+
