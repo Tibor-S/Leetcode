aa80a548cbeda3dbef638f1543c243a4afeab1e5
Sebastian Sandstig <sebastian.sandstig@gmail.com>
2026-04-19 19:33:02 +0200

    50

---
+++ 50.pow-x-n.cpp
@@ -0,0 +1,16 @@
+#include <cmath>
+/*
+ * @lc app=leetcode id=50 lang=cpp
+ *
+ * [50] Pow(x, n)
+ */
+
+// @lc code=start
+class Solution {
+public:
+    double myPow(double x, int n) {
+        return std::pow(x, n);
+    }
+};
+// @lc code=end
+
