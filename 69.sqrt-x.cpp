916a26f0bc8230181b862dfaa638e18e27b658ad
Sebastian Sandstig <sebastian.sandstig@gmail.com>
2026-04-22 19:13:56 +0200

    69

---
+++ 69.sqrt-x.cpp
@@ -0,0 +1,16 @@
+#include <cmath>
+/*
+ * @lc app=leetcode id=69 lang=cpp
+ *
+ * [69] Sqrt(x)
+ */
+
+// @lc code=start
+class Solution {
+public:
+    int mySqrt(int x) {
+        return std::sqrt(x);
+    }
+};
+// @lc code=end
+
